bad37de5f600a8263fe8bd63c9858917a2c159eb
Lucas Passos <lucasplpx@gmail.com>
2021-06-24 21:01:21 -0300

    04 - legacy

--- src/router.tsx
+++ src/router.tsx
@@ -4,6 +4,7 @@ import { AuthProvider } from './contexts/AuthContext';
 import Home from './pages/Home';
 import NewRoom from './pages/NewRoom';
 import Room from './pages/Room';
+import AdminRoom from './pages/AdminRoom';
 
 const Router = () => {
   return (
@@ -13,6 +14,7 @@ const Router = () => {
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthProvider>
     </BrowserRouter>
